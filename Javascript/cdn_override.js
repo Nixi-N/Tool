e2d309d7dc8f576a5335e5072b3b4b8843ce0be6
Weiße Teesorte <160199377+Nixi-N@users.noreply.github.com>
2025-06-12 18:03:11 +0800

    Delete Javascript directory

--- Javascript/cdn_override.js
+++
@@ -1,8 +0,0 @@
-const originalHost = "iosapps.itunes.apple.com";
-const newHost = "iosapps.itunes.apple.com.download.ks-cdn.com";
-if ($request.headers.Host === originalHost) {
-  $request.headers.Host = newHost;
-  console.log(`[CDN Override] Host header rewritten from ${originalHost} to ${newHost}`);
-}
-
-$done({});
